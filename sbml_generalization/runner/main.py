28fdb6f5e865674719209f5dd067ebe29ba1dccf
Anna Zhukova <zhutchok@gmail.com>
2016-08-25 00:32:34 +0200

    refactoring: clean argument parsing

--- sbml_generalization/runner/main.py
+++ sbml_generalization/runner/main.py
@@ -1,68 +1,29 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import getopt
+import glob
 import logging
-from os import listdir
-import sys
+import os
 
 import libsbml
 import openpyxl
 import openpyxl.styles
 from openpyxl.styles import Style, Font
-from openpyxl.styles.colors import Color
 
 from mod_sbml.annotation.chebi.chebi_annotator import get_chebi_term_by_annotation
-from mod_sbml.sbml.sbml_manager import get_gene_association
-from sbml_generalization.merge.model_merger import merge_models
-from sbml_generalization.generalization.sbml_generalizer import generalize_model
 from mod_sbml.annotation.chebi.chebi_serializer import get_chebi
 from mod_sbml.onto import parse_simple
-from mod_sbml.utils.misc import invert_map
+from mod_sbml.sbml.sbml_manager import get_gene_association
 from mod_sbml.serialization import get_sbml_r_formula
+from mod_sbml.utils.misc import invert_map
+from sbml_generalization.generalization.sbml_generalizer import generalize_model
+from sbml_generalization.merge.model_merger import merge_models
 
 __author__ = 'anna'
-
-##
-# runner module generalizes the model.
-# usage: main.py --model model.xml --chebi chebi.obo --verbose
-##
-
-help_message = '''
-Generalizes the model.
-usage: main.py --model model.xml --verbose
-'''
 
 BASIC_STYLE = Style(font=Font(color=openpyxl.styles.colors.BLACK, sz=8))
 BOLD_STYLE = Style(font=Font(bold=True, sz=8))
 HEADER_STYLE = BOLD_STYLE
-
-
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv
-    try:
-        chebi, in_sbml, in_path, out_sbml, groups_sbml, merged_sbml, verbose, log_file = process_args(argv)
-        if verbose:
-            logging.basicConfig(level=logging.INFO)
-        logging.info("parsing ChEBI...")
-        ontology = parse_simple(chebi)
-        if not in_sbml and in_path:
-            in_sbml_list = ['%s/%s' % (in_path, f) for f in listdir(in_path)
-                            if f.find(".xml") != -1 or f.find(".sbml") != -1]
-            for sbml in in_sbml_list:
-                doc = libsbml.SBMLReader().readSBML(sbml)
-                model = doc.getModel()
-                print(sbml, model.getNumSpecies(), model.getNumReactions())
-            merge_models(in_sbml_list, merged_sbml)
-            in_sbml = merged_sbml
-        r_id2clu, s_id2clu, _, _ = generalize_model(groups_sbml, out_sbml, in_sbml, ontology, ub_chebi_ids={'chebi:ch'})
-        if in_path:
-            serialize_generalization(r_id2clu, s_id2clu, groups_sbml, ontology, '%s/generalized.xlsx' % in_path)
-    except Usage, err:
-        logging.error(sys.argv[0].split("/")[-1] + ": " + str(err.msg))
-        logging.error(sys.stderr, "\t for help use --help")
-        return 2
 
 
 def add_values(ws, row, col, values, style=BASIC_STYLE):
@@ -138,67 +99,55 @@ def serialize_generalization(r_id2clu, s_id2clu, sbml, chebi, path):
     wb.save(path)
 
 
-def generate_out_sbml_name(in_sbml, in_path, out_sbml, groups_sbml, merged_sbml):
-    if not in_sbml and not in_path:
-        raise Usage(help_message)
-    if in_sbml:
-        extension = in_sbml.find(".xml")
-        if extension == -1:
-            extension = in_sbml.find(".sbml")
-        if extension == -1:
-            extension = len(in_sbml)
-        prefix = in_sbml[:extension]
-    else:
-        prefix = '{0}/merged'.format(in_path)
-        if not merged_sbml:
-            merged_sbml = "{0}.xml".format(prefix)
-    if not out_sbml:
-        out_sbml = "{0}_generalized.xml".format(prefix)
-    if not groups_sbml:
-        groups_sbml = "{0}_with_groups.xml".format(prefix)
-    return out_sbml, groups_sbml, merged_sbml
-
-
-def process_args(argv):
-    try:
-        opts, args = getopt.getopt(argv[1:], "m:c:h:o:v:l:p:g:b",
-                                   ["help", "model=", "chebi=", "outmodel=", "verbose", "log=", "inpath=", "groupsmodel=",
-                                    "mergedmodel="])
-    except getopt.error, msg:
-        raise Usage(msg)
-    in_path, in_sbml, groups_sbml, merged_sbml, chebi, out_sbml, verbose, log_file = \
-        None, None, None, None, None, None, False, None
-    # option processing
-    for option, value in opts:
-        if option in ("-h", "--help"):
-            raise Usage(help_message)
-        if option in ("-m", "--model"):
-            in_sbml = value
-        if option in ("-c", "--chebi"):
-            chebi = value
-        if option in ("-o", "--outmodel"):
-            out_sbml = value
-        if option in ("-g", "--groupsmodel"):
-            groups_sbml = value
-        if option in ("-b", "--mergedmodel"):
-            merged_sbml = value
-        if option in ("-v", "--verbose"):
-            verbose = True
-        if option in ("-l", "--log"):
-            log_file = value
-        if option in ("-p", "--inpath"):
-            in_path = value
-    out_sbml, groups_sbml, merged_sbml = generate_out_sbml_name(in_sbml, in_path, out_sbml, groups_sbml, merged_sbml)
-    if not chebi:
-        chebi = get_chebi()
-    if not in_sbml and not in_path:
-        raise Usage(help_message)
-    return chebi, in_sbml, in_path, out_sbml, groups_sbml, merged_sbml, verbose, log_file
-
-
-class Usage(Exception):
-    def __init__(self, msg):
-        self.msg = msg
-
 if __name__ == "__main__":
-    sys.exit(main())
+
+    # parameter parsing #
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Generalizes an SBML model.")
+    parser.add_argument('--model', required=True, type=str,
+                        help="input model in SBML format "
+                             "or a directory containing several models if they first need to be merged")
+    parser.add_argument('--chebi', default=None, type=str, help="path to the ChEBI ontology file in OBO format")
+    parser.add_argument('--output_model', default=None, type=str,
+                        help="path to the output generalized model in SBML format")
+    parser.add_argument('--groups_model', default=None, type=str,
+                        help="path to the output model in SBML format with groups extension to encode similar elements")
+    parser.add_argument('--merged_model', default=None, type=str,
+                        help="path to the output merged model in SBML format")
+    parser.add_argument('--verbose', action="store_true", help="print logging information")
+    parser.add_argument('--log', default=None, help="a log file")
+    params = parser.parse_args()
+
+    if not params.chebi:
+        params.chebi = get_chebi()
+    prefix = os.path.splitext(params.model)[0]
+    if not params.merged_model:
+        params.merged_model = "%s.xml" % prefix
+    if not params.output_model:
+        params.output_model = "%s_generalized.xml" % prefix
+    if not params.groups_model:
+        params.groups_model = "%s_with_groups.xml" % prefix
+
+    if params.verbose:
+        logging.basicConfig(level=logging.INFO)
+
+    logging.info("parsing ChEBI...")
+    ontology = parse_simple(params.chebi)
+    if os.path.isdir(params.model):
+        in_sbml_list = ['%s/%s' % (params.model, f) for f in glob.glob(os.path.join(params.model, '*'))
+                        if os.path.splitext(f)[1] in [".xml", ".sbml"]]
+        for sbml in in_sbml_list:
+            doc = libsbml.SBMLReader().readSBML(sbml)
+            model = doc.getModel()
+            print(sbml, model.getNumSpecies(), model.getNumReactions())
+        merge_models(in_sbml_list, params.merged_model)
+        sbml = params.merged_model
+    else:
+        sbml = params.model
+    r_id2clu, s_id2clu, _, _ = generalize_model(params.groups_model, params.output_model, sbml, ontology,
+                                                ub_chebi_ids={'chebi:ch'})
+    if os.path.isdir(params.model):
+        serialize_generalization(r_id2clu, s_id2clu, params.groups_model, ontology,
+                                 os.path.join(params.model, 'generalized.xlsx'))
+
