0d4c08fba92098f34f65fd8ad30b30def6010e95
Anna Zhukova <zhutchok@gmail.com>
2016-10-04 18:12:14 +0200

    ChEBI updated

--- sbml_generalization/generalization/sbml_generalizer.py
+++ sbml_generalization/generalization/sbml_generalizer.py
@@ -47,7 +47,7 @@ def generalize_model(groups_sbml, out_sbml, in_sbml, onto, ub_s_ids=None, ub_che
     s_id2chebi_id = get_species_id2chebi_id(input_model)
     ub_s_ids, ub_chebi_ids = get_ub_elements(input_model, onto, s_id2chebi_id, ub_chebi_ids, ub_s_ids)
 
-    terms = (onto.get_term(t_id) for t_id in s_id2chebi_id.itervalues())
+    terms = (t for t in (onto.get_term(t_id) for t_id in s_id2chebi_id.itervalues()) if t)
     old_onto_len = len(onto)
     filter_ontology(onto, terms, relationships=EQUIVALENT_RELATIONSHIPS, min_deepness=3)
     logging.info('Filtered the ontology from %d terms to %d' % (old_onto_len, len(onto)))
